8078afa35fad84e63757b6dc5c00a9acc2044149
tonyatpaypal <8559729+tonyatpaypal@users.noreply.github.com>
2020-03-19 21:35:30 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDBeekhydw4fzgJAGbIznV57aOPiimCzTsKcs41QAz+NPYbaAumAqNvWI4WagSBnsDns2CaIE7vNRLpXVuDQ6ql7p2F0xz8iRAYnJpinkoC2+Su+tPz8oD2zD76fSfbwZRwfoE9arKoxLTjYR5bJxv9Kw7bbi9lC/v/taBtunaMCpFzAWRRyXRv2AjOxEytWB8fMipjzIvvk6h6bAS4DlX7MxXco6dozSHMygX8+ZMfbiBhE5jIh38/hUN8nPWW+6z1HXkegNIV5glkOd15eA24//fzFsu4xV7PLerEwqB9rAPl+z/WOc8pAtx35eXDaouNOcL+tkkvT38X0wyRQP+l',
   }  
 }
