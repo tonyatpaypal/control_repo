0d6592806a6dc16f609c6a973f30477d90cf0e63
tonyatpaypal <8559729+tonyatpaypal@users.noreply.github.com>
2020-03-24 21:01:31 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,24 +1,24 @@
 class minecraft {
-  file{'/opt/minecraft':
+  file {‘/opt/minecraft’:
     ensure => directory,
-   }
-   file {'/opt/mincraft/minecraft_server.jar':
+  }
+  file {‘/opt/minecraft/minecraft_server.1.12.2.jar’:
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
-   }
-   package {'java':
+    source      => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+  }
+  package {‘java’:
     ensure => present,
-   }
-   file {'/opt/minecraft/eula.txt':
+  }
+  file {‘/opt/minecraft/eula.txt’:
     ensure => file,
-    content => 'eula=true',
-   }
-   file {/etc/systemd/system/minecraft.service':
+    content => ‘eula=true’
+  }
+  file {‘/etc/systemd/system/minecraft.service’:
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
-   }
-   service {'minecraft':
+    source => ‘puppet:///modules/minecraft/minecraft.service’,
+  }
+  service { ‘minecraft’:
     ensure => running,
     enable => true,
-   }
+  }
 }
